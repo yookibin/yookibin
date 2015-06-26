d9019ad2254f5ced8f0538da7f71dba968b5ea92
yookibin <yookibin@naver.com>
2015-06-26 17:15:14 +0900

    YouAndI_0626_kibin_ver05

--- date/src/main/webapp/css/board/script.js
+++ date/src/main/webapp/css/board/script.js
@@ -1,7 +1,8 @@
 function writeReply(root, board_num){
 	var reply=$("#reply").val();
 	//alert(reply+root+board_num);
-	
+	var url=root+"/memberboard/read.do?board_num="+board_num+"&reply="+reply;
+	alert(url);
 }
 
 function checkForm(boardForm){
